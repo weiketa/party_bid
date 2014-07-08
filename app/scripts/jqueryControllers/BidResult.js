9f1a89fee38ff2aa2480f5cb3492f3fbe5dc69f0
weiketa <lyvoctor@gmail.com>
2014-07-08 15:56:18 +0800

    升序显示竞价结果信息

--- app/scripts/jqueryControllers/BidResult.js
+++ app/scripts/jqueryControllers/BidResult.js
@@ -6,8 +6,8 @@
     $('#goback').attr('href','#/bidlist/'+current_activity);
     var bid_messages=Bid.get_bid_messages(localStorage.current_activity,localStorage.current_bid);
     $('span').text(localStorage.current_bid+'('+bid_messages.length+'人)');
-    //$('#bidlist').html('<li><h3>竞价信息</h3></li>');
-    _.each(bid_messages,function(bid){
+    var sortBy_bid_messages=_.sortBy(bid_messages,function(bid_message){return Math.min(bid_message.price)});
+    _.each(sortBy_bid_messages,function(bid){
         $('#bidlist').append('<li><div><h3><table><tr><td>'+bid.applyname+'</td></tr><tr><td width="100%">'+bid.phone+'</td><td>￥'+bid.price+'</td></tr></table></li>');
     });
 
